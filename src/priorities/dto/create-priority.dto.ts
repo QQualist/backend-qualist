6769e04295e9d01246f4c6b4fe362d3a93ed1743
ViniciusKuchnir <vina200902@gmail.com>
2024-05-26 20:45:51 -0300

    feat: Create priority DTO

--- src/priorities/dto/create-priority.dto.ts
+++ src/priorities/dto/create-priority.dto.ts
@@ -1,1 +1,23 @@
-export class CreatePriorityDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
+
+export class CreatePriorityDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20, { message: 'Priority name is too long' })
+  name: string;
+
+  @IsNumber()
+  @IsInt()
+  deadline: number;
+
+  @IsString()
+  @IsUUID()
+  user_uuid: string;
+}
