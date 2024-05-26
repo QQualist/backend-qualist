e54c6986f890aeda15c295104a2f66123d89f384
ViniciusKuchnir <vina200902@gmail.com>
2024-05-26 15:58:21 -0300

    feat: Create checklist DTO's

--- src/checklists/dto/create-checklist.dto.ts
+++ src/checklists/dto/create-checklist.dto.ts
@@ -1,1 +1,18 @@
-export class CreateChecklistDto {}
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateChecklistDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50, { message: 'Checklist name is too long' })
+  name: string;
+
+  @IsString()
+  @IsUUID()
+  user_uuid: string;
+}
--- src/checklists/dto/update-checklist.dto.ts
+++ src/checklists/dto/update-checklist.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateChecklistDto } from './create-checklist.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateChecklistDto extends PartialType(CreateChecklistDto) {}
+export class UpdateChecklistDto extends PartialType(CreateChecklistDto) {
+  @IsBoolean()
+  @IsOptional()
+  active: boolean; //Default value is TRUE
+}
