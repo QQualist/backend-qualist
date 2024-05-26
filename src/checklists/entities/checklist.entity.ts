8855b233a3ae61c229deb15effb9a43ad72277cf
ViniciusKuchnir <vina200902@gmail.com>
2024-05-26 15:50:55 -0300

    feat: Create checklist entity

--- src/checklists/entities/checklist.entity.ts
+++ src/checklists/entities/checklist.entity.ts
@@ -1,1 +1,42 @@
-export class Checklist {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'checklists' })
+export class Checklist {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  name: string;
+
+  @Column({ type: 'boolean', default: true, nullable: false })
+  active: boolean;
+
+  @Column({ type: 'int', default: 1, nullable: false })
+  version: number;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    precision: 0,
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    precision: 0,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'datetime', nullable: true })
+  deletedAt: Date;
+}
