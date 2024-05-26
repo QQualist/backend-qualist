84f4cc31fe8f8065cf1b4ed7ad4d37c87b1c6080
ViniciusKuchnir <vina200902@gmail.com>
2024-05-26 03:22:31 -0300

    feat: Create user DTO and entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,96 @@
-export class User {}
+import { Departament } from '../../departaments/entities/departament.entity';
+import { Role } from '../../roles/entities/role.entity';
+import { UserType } from '../../seeders/entities/user-type.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 30, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  surname: string;
+
+  @Column({ type: 'varchar', length: 60, unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @Column({ type: 'boolean', default: false, nullable: false })
+  canChangeQa: boolean;
+
+  @Column({ type: 'boolean', default: true, nullable: false })
+  sendNonConformitiesToEmail: boolean;
+
+  @ManyToOne(() => Departament, (departament) => departament, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'departament_uuid' })
+  departament: Departament;
+
+  @ManyToOne(() => User, (user) => user, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'creator_uuid' })
+  creator: User;
+
+  @ManyToOne(() => Role, (role) => role, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'role_uuid' })
+  role: Role;
+
+  @ManyToOne(() => UserType, (type) => type, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'user_type_id' })
+  type: UserType;
+
+  @ManyToOne(() => User, (user) => user, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'superior_uuid' })
+  superior: User;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    precision: 0,
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    precision: 0,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'datetime', nullable: true })
+  deletedAt: Date;
+}
