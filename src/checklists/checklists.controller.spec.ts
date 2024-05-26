6b741cac1d8bfa50f9b6047037a2efe6db7a1dea
ViniciusKuchnir <vina200902@gmail.com>
2024-05-26 20:24:32 -0300

    test: Config initial checklists tests

--- src/checklists/checklists.controller.spec.ts
+++ src/checklists/checklists.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChecklistsController } from './checklists.controller';
 import { ChecklistsService } from './checklists.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Checklist } from './entities/checklist.entity';
+import { UsersService } from '../users/users.service';
 
 describe('ChecklistsController', () => {
   let controller: ChecklistsController;
@@ -8,7 +11,17 @@ describe('ChecklistsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChecklistsController],
-      providers: [ChecklistsService],
+      providers: [
+        ChecklistsService,
+        {
+          provide: getRepositoryToken(Checklist),
+          useValue: {},
+        },
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<ChecklistsController>(ChecklistsController);
--- src/checklists/checklists.service.spec.ts
+++ src/checklists/checklists.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChecklistsService } from './checklists.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Checklist } from './entities/checklist.entity';
+import { UsersService } from '../users/users.service';
 
 describe('ChecklistsService', () => {
   let service: ChecklistsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChecklistsService],
+      providers: [
+        ChecklistsService,
+        {
+          provide: getRepositoryToken(Checklist),
+          useValue: {},
+        },
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<ChecklistsService>(ChecklistsService);
