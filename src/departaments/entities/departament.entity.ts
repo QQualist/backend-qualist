9e291f395d6534ab9e8f8039100e405dad062cb2
ViniciusKuchnir <vina200902@gmail.com>
2024-05-26 03:23:06 -0300

    feat: Create a departament entity

--- src/departaments/entities/departament.entity.ts
+++ src/departaments/entities/departament.entity.ts
@@ -1,1 +1,47 @@
-export class Departament {}
+import { User } from '../../users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'departaments' })
+export class Departament {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 25, nullable: false })
+  name: string;
+
+  @ManyToOne(() => Departament, (departament) => departament, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'user_uuid' })
+  user: User;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    precision: 0,
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    precision: 0,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'datetime', nullable: true })
+  deletedAt: Date;
+}
