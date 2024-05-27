ba3db69d6f2c9ba480cf5b132024bf53968241c8
ViniciusKuchnir <vina200902@gmail.com>
2024-05-27 14:39:35 -0300

    feat: Create item DTO

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,34 @@
-export class CreateItemDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255, { message: 'Description item is too long' })
+  description: string;
+
+  @IsString()
+  @MaxLength(255, { message: 'Risk is too long' })
+  @IsOptional()
+  risk: string | undefined;
+
+  @IsNumber()
+  @IsInt()
+  @IsOptional()
+  risk_type_id: string | undefined;
+
+  @IsString()
+  @IsUUID()
+  checklist_uuid: string;
+
+  @IsString()
+  @IsUUID()
+  priority_uuid: string;
+}
