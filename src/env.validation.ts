c6df3f0e78dd5f5cc5546ab478b53df22d957bf4
ViniciusKuchnir <vina200902@gmail.com>
2024-06-01 23:29:47 -0300

    fix: Validations .env

--- src/env.validation.ts
+++ src/env.validation.ts
@@ -1,29 +1,10 @@
 import { plainToInstance } from 'class-transformer';
-import { IsBoolean, IsInt, IsNumber, IsPort, IsString, IsStrongPassword, validateSync } from 'class-validator';
+import { IsString, validateSync } from 'class-validator';
 
 // Class responsible for checking that environment variables are set correctly
 class EnvironmentVariables {
   @IsString()
   JWT_SECRET: string;
-
-  @IsString()
-  MYSQL_DATABASE: string;
-
-  @IsString()
-  MYSQL_HOST: string;
-
-  @IsString()
-  MYSQL_PASSWORD: string;
-
-  @IsNumber()
-  @IsInt()
-  MYSQL_PORT: number;
-
-  @IsBoolean()
-  MYSQL_SSL: boolean;
-
-  @IsString()
-  MYSQL_USER: string;
 }
 
 export function validate(config: Record<string, unknown>) {
