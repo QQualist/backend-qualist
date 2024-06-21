1ded89a53c573f53703a834c9a9e78c428b86ed1
ViniciusKuchnir <vina200902@gmail.com>
2024-06-21 01:35:13 -0300

    feat: Create audit status entity

--- src/audit_status/entities/audit_status.entity.ts
+++ src/audit_status/entities/audit_status.entity.ts
@@ -1,1 +1,10 @@
-export class AuditStatus {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'audit_status' })
+export class AuditStatus {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 15, nullable: false, unique: true })
+  name: string;
+}
