08c1b20dbae2b584a9a9cc8ff16f3a0e51355666
ViniciusKuchnir <vina200902@gmail.com>
2024-06-21 17:41:12 -0300

    feat: Create audit entity

--- src/audits/entities/audit.entity.ts
+++ src/audits/entities/audit.entity.ts
@@ -1,1 +1,50 @@
-export class Audit {}
+import { AuditStatus } from '../../audit_status/entities/audit_status.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'audits' })
+export class Audit {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 25, nullable: false })
+  name: string;
+
+  @Column({ type: 'datetime', precision: 0, nullable: false })
+  date: Date;
+
+  @ManyToOne(() => AuditStatus, (audit_status) => audit_status, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'audit_status_id' })
+  audit_status_id: string;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    precision: 0,
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    precision: 0,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'datetime', nullable: true })
+  deletedAt: Date;
+}
