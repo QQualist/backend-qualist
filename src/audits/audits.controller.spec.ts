f0744003f69923b7d0196bf2063a5606a81ee728
ViniciusKuchnir <vina200902@gmail.com>
2024-06-23 00:59:07 -0300

    test: Config audit tests

--- src/audits/audits.controller.spec.ts
+++ src/audits/audits.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuditsController } from './audits.controller';
 import { AuditsService } from './audits.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Audit } from './entities/audit.entity';
 
 describe('AuditsController', () => {
   let controller: AuditsController;
@@ -8,7 +10,13 @@ describe('AuditsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuditsController],
-      providers: [AuditsService],
+      providers: [
+        AuditsService,
+        {
+          provide: getRepositoryToken(Audit),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<AuditsController>(AuditsController);
--- src/audits/audits.service.spec.ts
+++ src/audits/audits.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuditsService } from './audits.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Audit } from './entities/audit.entity';
 
 describe('AuditsService', () => {
   let service: AuditsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuditsService],
+      providers: [
+        AuditsService,
+        {
+          provide: getRepositoryToken(Audit),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<AuditsService>(AuditsService);
