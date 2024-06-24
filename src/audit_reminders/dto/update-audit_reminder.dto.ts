6fea6a6307c624061391a8f68a83101b5caea42e
ViniciusKuchnir <vina200902@gmail.com>
2024-06-24 02:18:48 -0300

    feat: Create audit_reminders DTOs

--- src/audit_reminders/dto/update-audit_reminder.dto.ts
+++ src/audit_reminders/dto/update-audit_reminder.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateAuditReminderDto } from './create-audit_reminder.dto';
+import { IsDate, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateAuditReminderDto extends PartialType(CreateAuditReminderDto) {}
+export class UpdateAuditReminderDto extends PartialType(
+  CreateAuditReminderDto,
+) {
+  @IsDate()
+  @Type(() => Date)
+  @IsNotEmpty()
+  @ApiProperty({ description: 'Date and time the notification was sent' })
+  date: Date;
+}
